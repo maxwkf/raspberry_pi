d9ea7c0b9146f11eb00777ad6a770dfbec5a2827
Max Wong <maxwkf@gmail.com>
2016-04-21 17:35:55 +0800

    DHT22 program

---
+++ lab_remaster/2_get_temperature/cookieDHT22.py
@@ -0,0 +1,22 @@
+import pigpio
+import DHT22
+from time import sleep
+
+pi = pigpio.pi()
+
+dht22 = DHT22.sensor(pi, 18)
+#dht22.trigger()
+
+sleepTime = 3
+
+def readDHT22():
+	dht22.trigger()
+	humidity = '%.2f' % (dht22.humidity())
+	temp = '%.2f' % (dht22.temperature())
+	return (humidity, temp)
+
+while True:
+	humidity, temperature = readDHT22()
+	print("Humidity is: " + humidity + "%")
+	print("Temperature is: " + temperature + "C")
+	sleep(sleepTime)
