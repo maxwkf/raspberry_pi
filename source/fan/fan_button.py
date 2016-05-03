a33875a249ae57762957ff012ec5f5fbcf91108a
Max Wong <maxwkf@gmail.com>
2016-05-03 15:56:47 +0800

    Fine tune for fan_button

--- source/fan/fan_button.py
+++ source/fan/fan_button.py
@@ -1,23 +1,34 @@
+###################################
+## Relay
+##	> s: (BCM) #12
+##	> +: 5.0V - Right 1st pin
+##	> -: GND below #12 
+##	> Com: Fan Moter Positive
+##	> NO: 5.0V - Right 2nd pin
+##
+## Fan Motor
+##	> +: Relay Com
+##	> -: GND - Right 3rd pin
+##################################
 #!/usr/bin/env python
 from devicehub.devicehub import Sensor, Actuator, Device, Project
 import threading
 from time import sleep
 import RPi.GPIO as GPIO
 import json
-PROJECT_ID = '6898' #projekct_id
-DEVICE_UUID = 'e615f24b-79ea-4526-abe4-f98acbf41c34' #device_uuid
-API_KEY = '1202a342-6b2c-4943-b783-338593ad4f56' #api_key
-ACTUATOR_NAME1 = 'led_button' #do_button
+PROJECT_ID = '7233' #projekct_id
+DEVICE_UUID = 'f95a6373-236b-4aeb-8736-e514f62a75e6' #device_uuid
+API_KEY = '48b6d932-d36a-4879-93a6-9a63cdf52b7c' #api_key
+ACTUATOR_NAME1 = 'fan_button' #do_button
+
+GPIO.setmode(GPIO.BCM)
 
 # init pin
 fan = 12
-
-GPIO.setmode(GPIO.BOARD)
 GPIO.setup(fan, GPIO.OUT)
 
 def fan_button(state):
 	GPIO.output(fan, state)
-	print " state: ", state
 
 def act1_callback(payload):
 	"""
@@ -25,8 +36,11 @@ def act1_callback(payload):
 	"""
 	# handles message arrived on subscribed topic
 	msg = str(payload)
-	act_state = ACT1.state
-	print "act1", act_state
+	act_state = int(ACT1.state)
+	if (act_state == 0):
+		print "Signal Off received... do nothing"
+	else:
+		print "Signal On received... turn on the fan!!"
 	fan_button(act_state)
 
 project = Project(PROJECT_ID)
